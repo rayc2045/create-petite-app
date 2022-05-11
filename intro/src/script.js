c74118e8b1060bd79667b4d3ea99cefe60cdbbb3
rayc2045 <>
2022-05-11 21:03:56 +0800

    Simplify code

--- intro/src/script.js
+++ intro/src/script.js
@@ -4,7 +4,8 @@ import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/petite-vue/0.4
 import { fetchData, copyText, toggleClasses } from '/src/scripts/utils.js';
 
 const App = {
-  isPrefersReducedMotion: window.matchMedia('(prefers-reduced-motion: reduce)').matches,
+  isPrefersReducedMotion: window.matchMedia('(prefers-reduced-motion: reduce)')
+    .matches,
   isLoading: true,
   animations: [],
   async init() {
@@ -12,7 +13,9 @@ const App = {
     this.isLoading = false;
   },
   copyText,
-  toggleClasses,
+  toggleAnimation(el, animation) {
+    toggleClasses(el, `animate__animated animate__${animation} animate__infinite`);
+  },
 };
 
 createApp(App).mount();
