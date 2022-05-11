b7390c43f9bca983aa76f93dc090c27d5529a55c
rayc2045 <>
2022-05-11 22:40:21 +0800

    Optimize import

--- intro/src/scripts/script.js
+++ intro/src/scripts/script.js
@@ -1,15 +1,27 @@
 'use strict';
 
-import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/petite-vue/0.4.1/petite-vue.es.min.js';
+import { createApp, reactive } from 'https://cdnjs.cloudflare.com/ajax/libs/petite-vue/0.4.1/petite-vue.es.min.js';
 import { fetchData, copyText, toggleClasses } from '/src/scripts/utils.js';
 
+const ClassesSearch = reactive({
+  classes: [],
+  currentSearch: '',
+  get searchResults() {
+
+  },
+  fetchBootstrapMinCss() {
+    this.classes = fetch('/src/styles/libraries/_bootstrap.scss')
+  }
+});
+
 const App = {
+  ClassesSearch,
   isPrefersReducedMotion: window.matchMedia('(prefers-reduced-motion: reduce)')
     .matches,
   isLoading: true,
   animations: [],
   async init() {
-    this.animations = await fetchData('./data/animations.json');
+    this.animations = await fetchData('./src/data/animations.json');
     this.isLoading = false;
   },
   copyText,
