63db6abcba474b9b059f3f42578ac66d7bdae93c
rayc2045 <>
2022-06-03 02:51:57 +0800

    Update demo

--- demo/src/scripts/script.js
+++ demo/src/scripts/script.js
@@ -16,12 +16,34 @@ import { getWindowWidth, toggleClasses, thousandFormat } from '/src/scripts/util
       '(prefers-reduced-motion: reduce)'
     ).matches,
     isLoading: true,
+    contents: ['project', 'posts', 'comments'],
+    currentContentIdx: 0,
+    posts: [],
+    comments: [],
     rewards: [],
     async init() {
       this.rewards = await fetch('/demo/src/data/rewards.json').then(res =>
         res.json()
       );
       this.isLoading = false;
+    },
+    async updateContent(idx) {
+      this.currentContentIdx = idx;
+      if (idx === 0) return;
+      if (idx === 1 && !this.posts.length) {
+        this.isLoading = true;
+        this.posts = await fetch('/demo/src/data/posts.json').then(res =>
+          res.json()
+        );
+        return this.isLoading = false;
+      }
+      if (idx === 2 && !this.comments.length) {
+        this.isLoading = true;
+        this.posts = await fetch('/demo/src/data/comments.json').then(res =>
+          res.json()
+        );
+        return this.isLoading = false;
+      }
     },
     toggleClasses,
     thousandFormat,
